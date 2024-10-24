d782907ba2b9d82e6c52b45e0ee57e6dcbcb9218
Nick Bohannan <nbohannan@gmail.com>
2024-10-23 19:49:12 -0500

    formatting

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ string filePath = "mainframe_ebcdic_file.dat";
 string outputFilePath = "ParsedErrors.txt";
 
 Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
 Encoding ebcdicEncoding = Encoding.GetEncoding("IBM037");
 
 var errorMessages = new List<string>();
@@ -18,17 +17,13 @@ try
 
     foreach (var line in fileLines)
     {
-        // Check if the line matches the error pattern
         Match match = Regex.Match(line, errorPattern);
         if (match.Success)
         {
-            Console.WriteLine("match found...");
-            // Add the captured error message to the list
             errorMessages.Add(match.Groups[1].Value);
         }
     }
 
-    // Write errors to the output file
     File.WriteAllLines(outputFilePath, errorMessages);
 
     Console.WriteLine($"Found {errorMessages.Count} error(s). Errors saved to {outputFilePath}");
